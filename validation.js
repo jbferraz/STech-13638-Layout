70af72396cb8d103dcd4b4e6548e44bfd9bbdb40
Jair Borges Ferraz <jbferraz@senacinformatica.br>
2022-06-28 17:25:38 -0300

    Validação com JS

--- validation.js
+++ validation.js
@@ -5,10 +5,62 @@ function validar(){
 	var telefone = formUser.telefone.value;
 	var endereco = formUser.endereco.value;
 	var senha = formUser.senha.value;
+	var cpf = formUser.cpf.value;
+	var cnpj = formUser.cnpj.value;
 	
 	if(nome =="" || nome.length <=3){
 		alert("Informe o nome completo!");
 		formUser.nome.focus();
+		return false;
+	}
+	
+	if(email == "" || email.length < 10){
+		alert("E-mail não atende os requisitos!");
+		formUser.email.focus();
+		return false;
+	}
+	
+	if(telefone == "" || telefone.length <= 10){
+		alert("Informe telefone com DDD!");
+		formUser.telefone.focus();
+		return false;
+	}
+	
+	if(endereco == "" || endereco.length <= 5){
+		alert("Informe endereço completo!");
+		formUser.endereco.focus();
+		return false;
+	}
+	
+	if(cpf == "" && cnpj == ""){
+		alert("Informe CPF ou CNPJ");
+		formUser.cpf.focus();
+		return false;
+	}
+	
+	if(cpf != "" && cnpj != ""){
+		alert("Escolha CPF ou CNPJ");
+		formUser.cpf.focus();
+		return false;
+	}
+	
+	if(cpf == "" && cnpj != ""){
+		if(cnpj.length != 18){
+			alert("CNPJ incorreto");
+			formUser.cnpj.focus();
+			return false;
+		}
+	}else if(cnpj == "" && cpf != ""){
+		if(cpf.length != 14){
+			alert("CPF inválido!");
+			formUser.cpf.focus();
+			return false;
+		}
+	}
+	
+	if(senha == "" || senha.length <= 6){
+		alert("Informe senha minimo 6 digitos!");
+		formUser.senha.focus();
 		return false;
 	}
 }
